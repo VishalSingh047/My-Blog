06b63ffb636301650904661a6c479eb16d458ac6
Vishal Singh <vs997131@gmail.com>
2025-07-13 07:30:47 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { useDispatch } from "react-redux";
 import "./App.css";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
-import { Footer, Header } from "./components";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 
 function App() {
