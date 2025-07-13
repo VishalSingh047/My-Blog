22566e1791d1e20c72db496cbaf2f70172dadd35
Vishal Singh <vs997131@gmail.com>
2025-07-13 09:00:20 +0530

    updated

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,31 +1,32 @@
-import React, {useEffect, useState} from 'react'
-import {Container, PostForm} from '../components'
+import React, { useEffect, useState } from "react";
+import PostForm from "../components/post-form/PostForm";
+import Container from "../components/container/Container";
 import appwriteService from "../appwrite/config";
-import { useNavigate,  useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 
 function EditPost() {
-    const [post, setPosts] = useState(null)
-    const {slug} = useParams()
-    const navigate = useNavigate()
+  const [post, setPosts] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (slug) {
-            appwriteService.getPost(slug).then((post) => {
-                if (post) {
-                    setPosts(post)
-                }
-            })
-        } else {
-            navigate('/')
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPosts(post);
         }
-    }, [slug, navigate])
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
   return post ? (
-    <div className='py-8'>
-        <Container>
-            <PostForm post={post} />
-        </Container>
+    <div className="py-8">
+      <Container>
+        <PostForm post={post} />
+      </Container>
     </div>
-  ) : null
+  ) : null;
 }
 
-export default EditPost
+export default EditPost;
