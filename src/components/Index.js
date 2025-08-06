b5f2863cfe331a691d001ea48e6be3d1f744f9d3
Vishal Singh <vs997131@gmail.com>
2025-08-07 01:10:06 +0530

    update

--- src/components/Index.js
+++ src/components/Index.js
@@ -12,7 +12,7 @@ import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Input from "./Input";
-export index =  {
+export {
     Header,
     Footer,
     Container,
